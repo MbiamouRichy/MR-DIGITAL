6c4aacc1f3755de60b7f023665fd4048e035baa2
Richy <richymbiamou52@gmail.com>
2022-11-23 19:28:10 +0100

    Mise en place de l'interface et des elements necessaires
    Creation du code html du code css
    Mise en place des image et svg

--- chatbox/src/app/app.component.ts
+++ chatbox/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatbox';
+  hover = 'nohover';
+  clic = 'noclic';
+  onchange(){
+    this.hover =  'hover';
+  }
+  onpaschange(){
+    this.hover =  'nohover';
+  }
+  onclic(){
+    this.clic = 'clic';
+  }
+  onnoclic(){
+    this.clic = 'noclic';
+  }
+
 }
