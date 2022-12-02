ef17f00713f98f895001c8a25779ac0f0a664b5e
Richy <richymbiamou52@gmail.com>
2022-12-02 10:43:32 +0100

    Amelioration du backend de la chatbox

--- chatbox/src/app/app.component.ts
+++ chatbox/src/app/app.component.ts
@@ -31,29 +31,13 @@ export class AppComponent implements  OnInit{
               private Router: Router) {
   }
 
-  /*
-reponse = ""
-onSend(name:string){
-  const formData : FormData = new FormData()
-  formData.append('name',name)
-  this.nameService.onSendService(formData).subscribe
-  (res=>{
-    console.log(res);
-    this.reponse = res
-  },
-   err=>{
-    console.log(err);
-   }
-  )
-}
-*/
-r1 = /bonjour/ig;
-r2 = /comment allez vous/ig;
-r3 = /je cherche a eleve mon chiffre d'affaire grace au digital/ig;
+  r1 = /bonjour/ig;
+  r2 = /comment allez vous/ig;
+  r3 = /je cherche a eleve mon chiffre d'affaire grace au digital/ig;
   ngOnInit(): void {
     this.getProductList();
     this.creatProduct();
-    this.getreponse()
+    this.getreponse();
   }
 
   p: any = [];
@@ -62,59 +46,87 @@ r3 = /je cherche a eleve mon chiffre d'affaire grace au digital/ig;
   getProductList() {
     this.nameService.loadProducts().subscribe(res => {
       this.p = res;
-      console.log('res', res);
-      console.log('push', this.p);
       return this.p;
     })
   }
   getreponse() {
     this.nameService.loadreponse().subscribe(resa => {
       this.resp = resa;
-      console.log('res', resa);
-      console.log('push', this.resp);
       return this.resp;
     })
   }
 
-creatProduct(){
+  creatProduct(){
     this.productForm = this.formBuilder.group({
       'message': ['', Validators.compose([Validators.required, Validators.minLength(1),
-      Validators.maxLength(500)])],
+        Validators.maxLength(500)])],
       'reponse': ['', Validators.compose([Validators.required, Validators.minLength(1),
         Validators.maxLength(10)])]
     });
-}
-
-creat(values: any, isUpdate:any){
- if (values.message.match(this.r1)){
-  values.reponse = 1;
-  console.log(values.reponse);
- }else if (values.message.match(this.r2)){
-   values.reponse = 2;
-   console.log(values.reponse);
- }else if (values.message.match(this.r3)){
-  values.reponse = 3;
-  console.log(values.reponse);
-  }else
-{
-  values.reponse = 4;
-}
-  console.log(values.reponse);
-  let formData = new FormData();
-  formData.append('message', values.message);
-  formData.append('reponse', values.reponse);
-  if (isUpdate){
-   // for update
-    console.log('ca existe deja');
-  } else {
-    this.nameService.creat(formData).subscribe(res => {
-      /* if(res.result === 'success') {
-          this.router.navigate(['A']) }*/
-      this.ngOnInit()
-      console.log(res);
-    },error => {console.log('erreur', error)});
   }
-}
 
+  creat(values: any, isUpdate:any){
+    if (values.message.match(this.r1)){
+      values.reponse = 1;
+    }else if (values.message.match(this.r2)){
+      values.reponse = 2;
+    }else if (values.message.match(this.r3)){
+      values.reponse = 3;
+    }else
+    {
+      values.reponse = 4;
+    }
+    let formData = new FormData();
+    formData.append('message', values.message);
+    formData.append('reponse', values.reponse);
+    if (isUpdate){
+      // for update
+      console.log('ca existe deja');
+    } else {
+      this.nameService.creat(formData).subscribe(res => {
+        /* if(res.result === 'success') {
+            this.router.navigate(['A']) }*/
+        let overflow = document.querySelector('#overflow');
+        // @ts-ignore
+        let matelement:any;
+        let compte = 0;
+        this.getProductList()
+        const interval = setInterval(
+          ()=>{
+            // @ts-ignore
+            let child = overflow.firstElementChild;
+            // @ts-ignore
+            let mat = child.lastElementChild;
+            // @ts-ignore
+            let mat1 = mat.firstElementChild;
+            // @ts-ignore
+            let mat2 = mat.lastElementChild;
+            // @ts-ignore
+            mat1.classList.add('d-none')
+            // @ts-ignore
+            mat2.classList.remove('d-none')
+            compte++;
+            if (compte == 20){
+              clearInterval(interval)
+              // @ts-ignore
+              mat2.classList.add('d-none')
+              // @ts-ignore
+              mat1.classList.remove('d-none')
+            }
+          },100
+        )
+      },error => {console.log('erreur', error)});
+    }
+  }
+
+  re: any = []
+  affiche(p:any){
+    for (let i of this.resp){
+      if (i.id_reponse == p){
+        this.re =  i
+        return this.re
+      }
+    }
+  }
 
 }
