5bd9fa6c82444f73789fd488da8394d83c5658cd
Richy <richymbiamou52@gmail.com>
2022-12-06 13:36:31 +0100

    J'ameliore le backend de la chatbox

--- chatbox/src/app/app.component.ts
+++ chatbox/src/app/app.component.ts
@@ -31,9 +31,7 @@ export class AppComponent implements  OnInit{
               private Router: Router) {
   }
 
-  r1 = /bonjour/ig;
-  r2 = /comment allez vous/ig;
-  r3 = /je cherche a eleve mon chiffre d'affaire grace au digital/ig;
+
   ngOnInit(): void {
     this.getProductList();
     this.creatProduct();
@@ -56,6 +54,17 @@ export class AppComponent implements  OnInit{
     })
   }
 
+  re: any = []
+  affiche(p:any){
+    for (let i of this.resp){
+      if (i.id_reponse == p){
+        this.re = i
+        console.log('re',this.re)
+        return this.re
+      }
+    }
+  }
+
   creatProduct(){
     this.productForm = this.formBuilder.group({
       'message': ['', Validators.compose([Validators.required, Validators.minLength(1),
@@ -64,17 +73,45 @@ export class AppComponent implements  OnInit{
         Validators.maxLength(10)])]
     });
   }
+  r1 = /bonjour/ig;
+  r2 = /Marketing/ig;
+  r3 = /probleme/ig;
+  r5 = /Non/ig;
+  r7 = /oui/ig;
+  r9 = /Strategi/ig;
+  r10 = /Visibilite/ig;
+  r11 = /Credibilite/ig;
+  r12 = /tous/ig;
+  r13 = /Vente/ig;
+  r14 = /^[1]/ig;
+  r15 = /Salut/ig;
+
 
   creat(values: any, isUpdate:any){
-    if (values.message.match(this.r1)){
+    if (values.message.match(this.r1) || values.message.match(this.r1)){
       values.reponse = 1;
-    }else if (values.message.match(this.r2)){
-      values.reponse = 2;
-    }else if (values.message.match(this.r3)){
-      values.reponse = 3;
-    }else
+    }else if (values.message.match(this.r14) || values.message.match(this.r2)){
+      values.reponse = 5;
+    }else if (values.message.match(this.r7) &&
+      this.re.message_reponse == "Avez vous les outils necessaires comme un site web , une application ou un reseau social"){
+      values.reponse = 6;
+    } else if ((values.message.match(this.r9) || values.message.match(this.r10)
+    || values.message.match(this.r11) || values.message.match(this.r12)
+    || values.message.match(this.r13)) && this.re.message_reponse == "Quelle difficulte rencontree vous parmi celles ci:\n" +
+      "A.Strategie\n" +
+      "B.Visibilite\n" +
+      "C.Credibilite\n" +
+      "D.Vente\n" +
+      "E.Au niveau du contenu\n" +
+      "F.Tous"){
+      values.reponse = 7;
+   }else if (values.message.match(this.r5) &&
+      this.re.message_reponse == "Avez vous les outils necessaires comme un site web , une application ou un reseau social "){
+      values.reponse = 8;
+    }
+    else
     {
-      values.reponse = 4;
+      values.reponse = 2;
     }
     let formData = new FormData();
     formData.append('message', values.message);
@@ -107,25 +144,12 @@ export class AppComponent implements  OnInit{
             mat2.classList.remove('d-none')
             compte++;
             if (compte == 20){
+              this.ngOnInit()
               clearInterval(interval)
-              // @ts-ignore
-              mat2.classList.add('d-none')
-              // @ts-ignore
-              mat1.classList.remove('d-none')
             }
           },100
         )
       },error => {console.log('erreur', error)});
-    }
-  }
-
-  re: any = []
-  affiche(p:any){
-    for (let i of this.resp){
-      if (i.id_reponse == p){
-        this.re =  i
-        return this.re
-      }
     }
   }
 
