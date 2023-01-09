ab987d4a75374a9a59ad0b0b9fc76c5bd368e3b5
Richy <richymbiamou52@gmail.com>
2023-01-09 22:42:33 +0100

    Amelioration du code lors de l'envoi  et de la validation de l'envoi

--- chatbox/src/app/app.component.ts
+++ chatbox/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NameService} from "./name.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -10,8 +9,6 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements  OnInit{
   title = 'crud';
-  // @ts-ignore
-  productForm: FormGroup;
   hover = 'nohover';
   clic = 'noclic';
 
@@ -28,15 +25,12 @@ export class AppComponent implements  OnInit{
   onnoclic(){
     this.clic = 'noclic';
   }
-  constructor(private nameService: NameService,
-              private  formBuilder: FormBuilder,
-              private Router: Router) {
+  constructor(private nameService: NameService) {
   }
 
 
   ngOnInit(): void {
     this.getProductList();
-    this.creatProduct();
     this.getreponse();
   }
 
@@ -66,14 +60,6 @@ export class AppComponent implements  OnInit{
     }
   }
 
-  creatProduct(){
-    this.productForm = this.formBuilder.group({
-      'message': ['', Validators.compose([Validators.required, Validators.minLength(1),
-        Validators.maxLength(500)])],
-      'reponse': ['', Validators.compose([Validators.required, Validators.minLength(1),
-        Validators.maxLength(10)])]
-    });
-  }
   r1 = /bonjour/ig;
   r2 = /Marketing/ig;
   r3 = /probleme/ig;
@@ -90,74 +76,66 @@ export class AppComponent implements  OnInit{
   res2 = /Quelle difficulte rencontree vous parmi celles ci/ig;
   res3 = /Nous vous proposons de creer vos outils/ig;
 
-  creat(values: any, isUpdate:any){
-
+  creat(form: NgForm){
     // @ts-ignore
     let message = document.querySelector('#message');
     // @ts-ignore
     let content = message.firstElementChild.textContent
-    if (values.message.match(this.r1) || values.message.match(this.r15)){
-      values.reponse = 1;
+    if (form.value.message.match(this.r1) || form.value.message.match(this.r15)){
+      form.value.reponse = 1;
     }
-    else if (values.message.match(this.r14) || values.message.match(this.r2)){
-      values.reponse = 5;
+    else if (form.value.message.match(this.r14) || form.value.message.match(this.r2)){
+      form.value.reponse = 5;
     }
     else
       // @ts-ignore
-    if (values.message.match(this.r7) && content.match(this.res1)){
-      values.reponse = 6;
+    if (form.value.message.match(this.r7) && content.match(this.res1)){
+      form.value.reponse = 6;
       }
     else
       // @ts-ignore
-    if (values.message.match(this.r7) && content.match(this.res3)){
-      values.reponse = 9;
+    if (form.value.message.match(this.r7) && content.match(this.res3)){
+      form.value.reponse = 9;
     }
     else
         // @ts-ignore
-      if ( content.match(this.res2) && (values.message.match(this.r9) || values.message.match(this.r10)
-          || values.message.match(this.r11) || values.message.match(this.r12)
-          || values.message.match(this.r13))){
-          values.reponse = 7;
+      if ( content.match(this.res2) && (values.message.match(this.r9) || form.value.message.match(this.r10)
+          || form.value.message.match(this.r11) || form.value.message.match(this.r12)
+          || form.value.message.match(this.r13))){
+          form.value.reponse = 7;
       }
     else
         // @ts-ignore
-        if (values.message.match(this.r5) && content.match(this.res1)) {
-              values.reponse = 8;
+        if (form.value.message.match(this.r5) && content.match(this.res1)) {
+              form.value.reponse = 8;
             }
 
     else {
-            values.reponse = 2;
+            form.value.reponse = 2;
       }
-
-
     let formData = new FormData();
-    formData.append('message', values.message);
-    formData.append('reponse', values.reponse);
-    if (isUpdate){
-      // for update
-      console.log('ca existe deja');
-    } else {
+    formData.append('message', form.value.message);
+    formData.append('reponse', form.value.reponse);
       this.nameService.creat(formData).subscribe(res => {
-        /* if(res.result === 'success') {
-            this.router.navigate(['A']) }*/
+
         let texte_env = document.querySelector('.saisissez')
         // @ts-ignore
         texte_env.value = ''
-        let overflow = document.querySelector('#overflow');
-        // @ts-ignore
-        let matelement:any;
         let compte = 0;
         this.getProductList()
         const interval = setInterval(
           ()=>{
             // @ts-ignore
-            let child = overflow.firstElementChild;
+            let mat = document.getElementById('message');
+            let overflow = document.getElementById('overflow')
             // @ts-ignore
-            let mat = child.lastElementChild;
+            child = overflow.lastElementChild
             // @ts-ignore
-            let mat1 = mat.firstElementChild;
+            child.classList.add('bg-dark')
             // @ts-ignore
-            let mat2 = mat.lastElementChild;
+            let mat1 = mat.firstElementChild
+            // @ts-ignore
+            let mat2 = mat.lastElementChild
             // @ts-ignore
             mat1.classList.add('d-none')
             // @ts-ignore
@@ -170,10 +148,30 @@ export class AppComponent implements  OnInit{
               mat1.classList.remove('d-none')
               clearInterval(interval)
             }
+
           },100
         )
       },error => {console.log('erreur', error)});
+    form.reset();
+  }
+  listen() {
+    let label = document.getElementById('label');
+    let input = document.querySelector('.saisissez')
+    // @ts-ignore
+    if (input.value == "") {
+      let c = 0;
+      const set = setInterval(
+        () => {
+          // @ts-ignore
+          input.classList.add('erreur')
+          c++;
+          if (c == 10) {
+            clearInterval(set)
+            // @ts-ignore
+            input.classList.remove('erreur')
+          }
+        }, 100
+      )
     }
   }
-
 }
