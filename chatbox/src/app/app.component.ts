2bbeaf68c7044b95d8b871188513f7bb1e482a9a
Richy <richymbiamou52@gmail.com>
2023-05-15 02:06:33 +0100

    update

--- chatbox/src/app/app.component.ts
+++ chatbox/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {NameService} from "./name.service";
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -7,157 +6,68 @@ import {NgForm} from "@angular/forms";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
-  title = 'crud';
+export class AppComponent {
+  title = 'Chatbox';
   hover = 'nohover';
   clic = 'noclic';
 
-  last = new Date();
+  messages = [
+    {message: "Hello!", class: "texte-du-chat-blue"},
+    {message: "How are you?", class: "texte-du-chat-blue"},
+    {message: "Please What is your name?", class: "texte-du-chat-blue"},
+    {message: "Hey! Fine thank you", class: "texte-du-chat-gris"},
+    {message: "I am Mbiamou Richy", class: "texte-du-chat-gris"},
+    {message: "I am frontend developper and web integrator. I created beautifull and usefull website for you.", class: "texte-du-chat-gris"},
+    {message: "Okay!", class: "texte-du-chat-blue"},
+    {message: "How can i help you?", class: "texte-du-chat-gris"}
+  ]
 
   onchange() {
     this.hover = 'hover';
   }
-
   onpaschange() {
     this.hover = 'nohover';
   }
-
   onclic() {
     this.clic = 'clic';
   }
-
   onnoclic() {
     this.clic = 'noclic';
   }
 
-  constructor(private nameService: NameService) {
-  }
-
-
-  ngOnInit(): void {
-    this.getSendList();
-    this.getreponse();
-  }
-
-  p: any = [];
-  resp: any = [];
-
-  getSendList() {
-    this.nameService.loadProducts().subscribe(res => {
-      this.p = res;
-      return this.p;
-    })
-  }
-
-  getreponse() {
-    this.nameService.loadreponse().subscribe(resa => {
-      this.resp = resa;
-      return this.resp;
-    })
-  }
-
-  re: any = []
-
-  affiche(p: any) {
-    for (let i of this.resp) {
-      if (i.id_reponse == p) {
-        this.re = i
-        return this.re
+  scroll() {
+    let overflow = document.getElementById("overflow") as HTMLDivElement
+    let block = document.querySelector(".block-message") as HTMLDivElement
+    let input = document.querySelector(".saisissez") as HTMLDivElement
+    let y =  block.offsetHeight as number
+    let count = overflow.scrollTop
+    let set = setInterval(() =>{
+      count++
+      overflow.scrollTo(0, count)
+      if(count == y){
+        clearInterval(set)
       }
-    }
+    }, 1)
+    input.focus()
   }
-
-  r1 = /bonjour/ig;
-  r2 = /Marketing/ig;
-  r3 = /probleme/ig;
-  r5 = /Non/ig;
-  r7 = /oui/ig;
-  r9 = /Strategi/ig;
-  r10 = /Visibilite/ig;
-  r11 = /Credibilite/ig;
-  r12 = /tous/ig;
-  r13 = /Vente/ig;
-  r14 = /^[1]/ig;
-  r15 = /Salut/ig;
-  r16 = /hey/ig;
-  res1 = /Avez vous les outils necessaires comme un site web , une application ou un reseau social/ig;
-  res2 = /Quelle difficulte rencontree vous parmi celles ci/ig;
-  res3 = /Nous vous proposons de creer vos outils/ig;
-
   creat(form: NgForm) {
-    if (form.value.message.match(this.r1) || form.value.message.match(this.r15) || form.value.message.match(this.r16)) {
-      form.value.reponse = 1;
-    } else {
-      // @ts-ignore
-      let message = document.querySelector('#message');
-      // @ts-ignore
-      let content = message.firstElementChild.textContent
-
-      if (form.value.message.match(this.r14) || form.value.message.match(this.r2)) {
-        form.value.reponse = 5;
-      } else
-        // @ts-ignore
-      if (form.value.message.match(this.r7) && content.match(this.res1)) {
-        form.value.reponse = 6;
-      } else
-        // @ts-ignore
-      if (form.value.message.match(this.r7) && content.match(this.res3)) {
-        form.value.reponse = 9;
-      } else
-        // @ts-ignore
-      if (content.match(this.res2) && (form.value.message.match(this.r9) || form.value.message.match(this.r10)
-        || form.value.message.match(this.r11) || form.value.message.match(this.r12)
-        || form.value.message.match(this.r13))) {
-        form.value.reponse = 7;
-      } else
-        // @ts-ignore
-      if (form.value.message.match(this.r5) && content.match(this.res1)) {
-        form.value.reponse = 8;
-      } else {
-        form.value.reponse = 2;
+    const message = {message: form.value.message, class: "texte-du-chat-blue"}
+    this.messages.push(message)
+    const blagues = [{message: "Ahhhhh!!", class: "texte-du-chat-gris"},
+      {message: "C'est une fausse chatbox!!", class: "texte-du-chat-gris"}]
+    blagues.forEach(blague =>{
+     let set = setInterval(() =>{
+       this.messages.push(blague)
+       clearInterval(set)
+      }, 1000)
       }
-    }
-    let formData = new FormData();
-    formData.append('message', form.value.message);
-    formData.append('reponse', form.value.reponse);
-    this.nameService.creat(formData).subscribe(res => {
-      let div = document.querySelector('.row1-div2-c')
-      // @ts-ignore
-      div.classList.remove('click')
-    }, error => {
-      console.log('erreur', error)
-    });
-    form.reset();
-
-    let compte = 0;
-    this.getSendList()
-    const interval = setInterval(
-      () => {
-        // @ts-ignore
-        let mat = document.getElementById('message');
-        // @ts-ignore
-        let mat1 = mat.firstElementChild
-        // @ts-ignore
-        let mat2 = mat.lastElementChild
-        // @ts-ignore
-        mat1.classList.add('d-none')
-        // @ts-ignore
-        mat2.classList.remove('d-none')
-        compte++;
-        if (compte == 20) {
-          // @ts-ignore
-          mat2.classList.add('d-none')
-          // @ts-ignore
-          mat1.classList.remove('d-none')
-          clearInterval(interval)
-        }
-
-      }, 100
     )
+
+    form.reset()
+    this.scroll()
   }
 
   listen() {
-    let label = document.getElementById('label');
     let div = document.querySelector('.row1-div2-c') as HTMLElement
     div.classList.add('click')
     let input = document.querySelector('.saisissez') as HTMLInputElement;
